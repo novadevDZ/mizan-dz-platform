d2e7c8caf07fd50c1b248a58752caba69e8ff609
bakhti Mohamed <bakhti.system.dev@gmail.com>
2026-09-04 20:41:09 +0100

    fix 1

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,12 +1,16 @@
-import { createAuthClient } from "better-auth/react";
+import {createAuthClient} from "better-auth/react";
 import {
     inferAdditionalFields,
     organizationClient,
 } from "better-auth/client/plugins";
 
-import type { auth } from "@/src/lib/auth";
+import type {auth} from "@/src/lib/auth";
 
 export const authClient = createAuthClient({
+    baseURL:
+        process.env.NEXT_PUBLIC_APP_URL ??
+        "http://localhost:3000",
+
     plugins: [
         inferAdditionalFields<typeof auth>(),
         organizationClient(),
